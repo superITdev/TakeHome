f183cbe33884fca2640dc4e2b247599912c53176
dsengineer <darren.smith@alectio.com>
2022-04-05 07:48:49 -0400

    simply e2e test

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,11 +1,8 @@
 // e2e test, make it very simple right now with only navigation test
 describe('Navigation', () => {
     it('should navigate to the posts page', () => {
-        // Start from the index page
+        // Start from the index page -> redirected to '/posts'
         cy.visit('/')
-
-        // Find a link with an href attribute containing "posts" and click it
-        cy.get('a[href*="posts"]').click()
 
         // The new url should include "/posts"
         cy.url().should('include', '/posts')
